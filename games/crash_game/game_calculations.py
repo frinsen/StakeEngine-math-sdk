a646060027e1b3bb024e37be5394cdcaff4e14dd
Martin Frindt <martinfrindt@frinsen-MacBookPro2023.local>
2025-12-18 08:17:48 +0100

    remove: drop crash_game (keeping in private main repo)

--- games/crash_game/game_calculations.py
+++
@@ -1,8 +0,0 @@
-"""Game calculations for Crash Game."""
-
-from src.executables.executables import Executables
-
-
-class GameCalculations(Executables):
-    """Crash game calculation engine."""
-    pass
--- games/crash_game/game_events.py
+++
@@ -1,3 +0,0 @@
-"""Game events for Crash Game."""
-
-from src.events.events import *
